963f0c54f2395dbdd0d0f660546392ba8be5be73
IstaCiganytelep <horvath.istvan.ocsi@gmail.com>
2023-05-04 14:08:17 +0200

    Beolvasas kesz

--- 3szog.py
+++ 3szog.py
@@ -1,4 +1,10 @@
 #haromszog
-a=int(input(("Add meg a háromszög 'a' oldalát"))
-b=int(input(("Add meg a háromszög 'b' oldalát"))
-c=int(input(("Add meg a háromszög 'c' oldalát"))
+a=int(input("Add meg a háromszög 'a' oldalát"))
+b=int(input("Add meg a háromszög 'b' oldalát"))
+c=int(input("Add meg a háromszög 'c' oldalát"))
+
+if c<a and a<b+c and b<a+c:
+	print ("a(z)",a,",",b,",",c,"3szögetalkot")
+else:
+	print ("a(z)",a,",",b,",",c," 3szöge nem talkot")
+	
